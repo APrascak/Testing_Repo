43a9f14b62fa5315ac6c2371c9fe4d14b970f7c1
patriels <patriels@users.noreply.github.com>
2019-03-01 16:20:12 -0500

    Second try

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,6 @@
 /* register the modules the application depends upon here*/
 angular.module('listings', []);
 
-/*Comment for test*/
+/*Comment for test*, 2?/
 /* register the application and inject all the necessary dependencies */
 var app = angular.module('directoryApp', ['listings']);
